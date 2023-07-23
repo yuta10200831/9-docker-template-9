b4a92b55bf63c1b088fe7f46e494a77eefd53ebc
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-07-23 14:55:41 +0900

    クラスにトライしようステップ2　課題②　提出

--- src/public/Q2.php
+++ src/public/Q2.php
@@ -1,6 +1,28 @@
 <?php
-$total = 0;
-for ($i = 0; $i < 15; $i++) {
-    $total += $i;
+class TotalNumber {
+    private $total;
+
+    public function __construct() {
+        $this->total = 0;
+    }
+
+    public function calculateTotal($limit) {
+        for ($i = 0; $i < $limit; $i++) {
+            $this->total += $i;
+        }
+        return $this->total;
+    }
 }
+
+$totalNumber = new TotalNumber();
+$limit = 15;
+$total = $totalNumber->calculateTotal($limit);
 var_dump($total);
+
+
+
+// $total = 0;
+// for ($i = 0; $i < 15; $i++) {
+//     $total += $i;
+// }
+// var_dump($total);
